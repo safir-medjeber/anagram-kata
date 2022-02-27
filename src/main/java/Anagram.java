0f0b8e8a05b89f908a0b22f4cc10d64d6f684784
Safir MEDJEBER <safir.medjeber@gmail.com>
2022-02-27 20:05:06 +0100

    naming

--- src/main/java/Anagram.java
+++ src/main/java/Anagram.java
@@ -4,16 +4,16 @@ import java.util.stream.Collectors;
 public class Anagram {
 
     public boolean isAnagram(String word1, String word2) {
-        return !areDifferentLength(word1, word2) && areSameWordAfterAlphabeticalReordering(word1, word2);
+        return !areDifferentLength(word1, word2) && areSameWordAfterAlphabeticallyReordered(word1, word2);
     }
 
     private boolean areDifferentLength(String word1, String word2) {
         return word1.length() != word2.length();
     }
 
-    private boolean areSameWordAfterAlphabeticalReordering(String word1, String word2) {
-        String word1splitSorted = Arrays.stream(word1.split("")).sorted().collect(Collectors.joining());
-        String word2splitSorted = Arrays.stream(word2.split("")).sorted().collect(Collectors.joining());
-        return word1splitSorted.equals(word2splitSorted);
+    private boolean areSameWordAfterAlphabeticallyReordered(String word1, String word2) {
+        String word1AlphabeticallyReordered = Arrays.stream(word1.split("")).sorted().collect(Collectors.joining());
+        String word2AlphabeticallyReordered = Arrays.stream(word2.split("")).sorted().collect(Collectors.joining());
+        return word1AlphabeticallyReordered.equals(word2AlphabeticallyReordered);
     }
 }
